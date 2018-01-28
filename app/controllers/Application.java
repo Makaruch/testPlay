775e1d1fd6a0af71b8f8e4143aa1091e3a16072a
SShagin <s.shagin@open-s.info>
2018-01-28 22:50:43 +0300

    stream

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,9 @@ package controllers;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.routing.Broadcast;
+import akka.routing.RoundRobinPool;
 import akka.stream.Materializer;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
@@ -12,9 +15,12 @@ import streams.protocol.*;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Application extends Controller {
+
+    private final int PARALLELIZM = 10;
 
     @Inject
     Materializer materializer;
@@ -23,16 +29,18 @@ public class Application extends Controller {
     ActorSystem actorSystem;
 
     public Result index() {
+
+        ActorRef actorPool = actorSystem.actorOf(new RoundRobinPool(PARALLELIZM).props(BackPressureActor.props()), "actor" + Math.random());
         ActorRef actor = actorSystem.actorOf(BackPressureActor.props(), "actor");
 
         List<String> lst = new ArrayList<>();
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 100; i++){
             lst.add(String.format("Message %d", i));
         }
 
         Source.from(lst)
             .map(ProcessMessage::new)
-            .runWith(Sink.actorRefWithAck(actor, new InitMessage(), AckMessage.getInstanse(), new CompleteMessage(), FailureMessage::new), materializer);
+            .runWith(Sink.actorRefWithAck(actorPool, new Broadcast(new InitMessage()), AckMessage.getInstanse(), new Broadcast(new CompleteMessage()), FailureMessage::new), materializer);
 
         return ok("ok");
     }
--- app/streams/BackPressureActor.java
+++ app/streams/BackPressureActor.java
@@ -12,10 +12,15 @@ import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class BackPressureActor extends AbstractActor {
+
+  private static AtomicInteger num = new AtomicInteger();
+
+  private int actornum;
 
   private static final ScheduledExecutorService scheduler =
       Executors.newScheduledThreadPool(
@@ -36,6 +41,10 @@ public class BackPressureActor extends AbstractActor {
     return timeoutCf;
   }
 
+  public BackPressureActor(){
+    this.actornum = num.incrementAndGet(); 
+  }
+
   static public Props props() {
     return Props.create(BackPressureActor.class, BackPressureActor::new);
   }
@@ -43,25 +52,23 @@ public class BackPressureActor extends AbstractActor {
   public Receive createReceive() {
     return receiveBuilder().
         match(InitMessage.class, msg -> {
-          Logger.info("Receive init message");
+          Logger.info("Receive init message in " + self().path().toSerializationFormat());
           sender().tell(AckMessage.getInstanse(), self());
         })
         .match(ProcessMessage.class, msg -> {
-          Logger.info(String.format("Receive process message :%s", msg.getMessage()));
+          Logger.info(String.format("Receive process message :%s in %s", msg.getMessage(), self().path().toSerializationFormat()));
           ActorRef sender = getSender();
           timeout(Duration.ofSeconds(5 + (long)(Math.random()* 10)), msg)
               .whenComplete( (done, thr)-> {
-                Logger.info(String.format("Complete message %s, sending ackMessage", done.getMessage()));
+                Logger.info(String.format("Complete message %s, sending ackMessage from %s", done.getMessage(), self().path().toSerializationFormat()));
                 sender.tell(AckMessage.getInstanse(), self());
               });
         })
         .match(CompleteMessage.class, msg -> {
-          Logger.info("Receive complete message, do roskomnadzor");
-          self().tell(PoisonPill.getInstance(), ActorRef.noSender());
+          Logger.info("Receive complete message in " + self().path().toSerializationFormat());
         })
         .match(FailureMessage.class, msg -> {
-          Logger.info("Receive failure message, do roskomnadzor anyway");
-          self().tell(PoisonPill.getInstance(), ActorRef.noSender());
+          Logger.info("Receive failure message in " + self().path().toSerializationFormat());
         })
         .build();
   }
